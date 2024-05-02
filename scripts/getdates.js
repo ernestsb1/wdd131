68d50c1e8715e7c726cb15d24356202d60f58250
ernestsb1 <ernestnyore2020@gmail.com>
2024-05-02 15:59:43 +0100

    Added

---
+++ scripts/getdates.js
@@ -0,0 +1,12 @@
+
+
+// Get the current year and display it in the <span> tag
+const currentYearElement = document.getElementById('currentyear');
+currentYearElement.textContent = new Date().getFullYear();
+
+// Update the last modification date
+const lastModifiedElement = document.getElementById('lastModified');
+lastModifiedElement.textContent = `Last modification: ${new Date('2024-04-29T11:34:45').toLocaleString()}`;
+
+
+  
