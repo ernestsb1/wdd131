e024fee8eb470651c8df060d98e8bc6b08352088
ernestsb1 <ernestnyore2020@gmail.com>
2024-05-07 01:32:59 +0100

    Added

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,11 +1,4 @@
-javascript
-// Update copyright year
-const copyrightYear = document.getElementById('currentyear');
-copyrightYear.textContent = new Date().getFullYear();
 
-// Update last modified date
-const lastModified = document.getElementById('last-modified');
-lastModified.textContent = new Date(document.lastModified).toLocaleString();
 
 // Hamburger menu functionality
 const hamburgerBtn = document.createElement('button');
@@ -19,3 +12,14 @@ hamburgerBtn.addEventListener('click', () => {
   nav.classList.toggle('show');
   hamburgerBtn.innerHTML = nav.classList.contains('show') ? '&#10006;' : '&#9776;';
 });
+
+
+const currentYearElement = document.getElementById('currentyear');
+currentYearElement.textContent = new Date().getFullYear();
+
+// Update the last modification date
+const lastModifiedElement = document.getElementById('lastModified');
+lastModifiedElement.textContent = `Last modification: ${new Date('2024-04-29T11:34:45').toLocaleString()}`;
+
+
+  
