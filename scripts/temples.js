ccc11de21035b472d6ba7233b655e5eda7cc2ece
ernestsb1 <ernestnyore2020@gmail.com>
2024-05-09 16:25:55 +0100

    Added

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,24 +1,14 @@
 
 
-// Hamburger menu functionality
-const hamburgerBtn = document.createElement('button');
-hamburgerBtn.classList.add('hamburger');
-hamburgerBtn.innerHTML = '&#9776;';
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("nav");
 
-const nav = document.querySelector('nav');
-nav.insertAdjacentElement('beforebegin', hamburgerBtn);
-
-hamburgerBtn.addEventListener('click', () => {
-  nav.classList.toggle('show');
-  hamburgerBtn.innerHTML = nav.classList.toggle('active') ? '&#10006;' : '&#9776;';
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
 
-//const hamburgerMenu = document.querySelector(".hamburger-menu");
-//const nav = document.querySelector("nav");
 
-//hamburgerMenu.addEventListener("click", () => {
-  //  nav.classList.toggle("active")
-//});
 
 
 const currentYearElement = document.getElementById("currentyear");
