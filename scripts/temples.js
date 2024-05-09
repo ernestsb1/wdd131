cf088ae530b7996f6a290a47143827837616326d
ernestsb1 <ernestnyore2020@gmail.com>
2024-05-09 13:35:48 +0100

    Added

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,25 +1,32 @@
 
 
 // Hamburger menu functionality
-const hamburgerBtn = document.createElement('button');
-hamburgerBtn.classList.add('hamburger');
-hamburgerBtn.innerHTML = '&#9776;';
+//const hamburgerBtn = document.createElement('button');
+//hamburgerBtn.classList.add('hamburger');
+//hamburgerBtn.innerHTML = '&#9776;';
 
-const nav = document.querySelector('nav');
-nav.insertAdjacentElement('beforebegin', hamburgerBtn);
+//const nav = document.querySelector('nav');
+//nav.insertAdjacentElement('beforebegin', hamburgerBtn);
 
-hamburgerBtn.addEventListener('click', () => {
-  nav.classList.toggle('show');
-  hamburgerBtn.innerHTML = nav.classList.contains('show') ? '&#10006;' : '&#9776;';
+//hamburgerBtn.addEventListener('click', () => {
+  //nav.classList.toggle('show');
+  //hamburgerBtn.innerHTML = nav.classList.contains('show') ? '&#10006;' : '&#9776;';
+//});
+
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const nav = document.querySelector(".nav");
+
+hamburgerMenu.addEventListener("click", () => {
+    nav.classList.toggle("active")
 });
 
 
-const currentYearElement = document.getElementById('currentyear');
+const currentYearElement = document.getElementById("currentyear");
 currentYearElement.textContent = new Date().getFullYear();
 
 // Update the last modification date
-const lastModifiedElement = document.getElementById('lastModified');
-lastModifiedElement.textContent = `Last modification: ${new Date('2024-04-29T11:34:45').toLocaleString()}`;
+const lastModifiedElement = document.getElementById("lastModified");
+lastModifiedElement.textContent = `Last modification: ${new Date("2024-04-29T11:34:45").toLocaleString()}`;
 
 
   
