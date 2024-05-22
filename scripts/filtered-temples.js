3d8fb56981f226a8b585541f9006e3eba623015e
ernestsb1 <ernestnyore2020@gmail.com>
2024-05-22 21:54:51 +0100

    Added

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -138,19 +138,6 @@ document.getElementById('filter-menu').addEventListener('click', event => {
 
 
 
-
-
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector("nav");
-
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
-
-
-
-
 const currentYearElement = document.getElementById("currentyear");
 currentYearElement.textContent = new Date().getFullYear();
 
